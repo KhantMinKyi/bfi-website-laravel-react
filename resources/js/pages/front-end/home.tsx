0dd737674898d11c43933169e8a14c83854576a1
khantminkyi <khantminkyi@gmail.com>
2025-11-01 10:19:08 +0630

    create footer design

--- resources/js/pages/front-end/home.tsx
+++ resources/js/pages/front-end/home.tsx
@@ -1,8 +1,8 @@
 import AboutTestimonial from '@/components/front-end/core/about-testimonial';
 import AccordionGallery from '@/components/front-end/core/accordion-gallery';
 import Counter from '@/components/front-end/core/counter';
+import { DotLoading } from '@/components/front-end/core/dot-loading';
 import EducationProgramme from '@/components/front-end/core/education-programme';
-import LoadingThreeDotsJumping from '@/components/front-end/core/loading-three-dots-jumping';
 import CarouselBanner from '@/components/front-end/home/carousel-banner';
 import HeroBanner from '@/components/front-end/home/hero-banner';
 import Information from '@/components/front-end/home/information';
@@ -17,32 +17,40 @@ function home() {
     const [loading, setLoading] = useState<boolean>(true);
 
     useEffect(() => {
-        fetch('https://dummy-json.mock.beeceptor.com/quotes?_limit=8')
+        // const timer = setTimeout(() => {
+        fetch('https://picsum.photos/v2/list')
             .then((res) => res.json())
             .then((data: Programmes[]) => {
                 setProgrammes(data);
                 setLoading(false);
             })
             .catch((err) => console.log(err));
+        // }, 2000); // 2-second delay
+
+        // return () => clearTimeout(timer); // cleanup on unmount
     }, []);
 
     return (
         <FrontEndLayout>
-            <CarouselBanner />
-            <SisterSchoolCards />
-            <Information />
-            <AccordionGallery />
-            {loading ? (
-                <div className="container mx-auto flex justify-center gap-10">
-                    <LoadingThreeDotsJumping />
-                </div>
-            ) : (
-                <EducationProgramme programmes={programmes} />
-            )}
-            <Counter />
-            <AboutTestimonial />
-            <HeroBanner />
-            <PhotoGallery />
+            <>
+                <CarouselBanner />
+                <SisterSchoolCards />
+                <Information />
+                <AccordionGallery />
+                {loading ? (
+                    <div className="container mx-auto flex justify-center gap-10">
+                        <div className="flex h-64 items-center justify-center text-lg text-gray-500">
+                            <DotLoading />
+                        </div>
+                    </div>
+                ) : (
+                    <EducationProgramme programmes={programmes} />
+                )}
+                <Counter />
+                <AboutTestimonial />
+                <HeroBanner />
+                <PhotoGallery />
+            </>
         </FrontEndLayout>
     );
 }
