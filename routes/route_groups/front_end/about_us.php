3de90dcfce72ff820c6abc6a1d258bfdd43da943
khantminkyi <khantminkyi@gmail.com>
2025-11-10 09:36:37 +0630

    create Philosophy Page

--- routes/route_groups/front_end/about_us.php
+++ routes/route_groups/front_end/about_us.php
@@ -9,5 +9,8 @@ Route::get('/about_us/our_history', function () {
 Route::get('/about_us/vision_mission_value', function () {
     return Inertia::render('front-end/about_us/VisionMissionValue');
 })->name('vision_mission_value');
+Route::get('/about_us/philosophy', function () {
+    return Inertia::render('front-end/about_us/OurPhilosophy');
+})->name('philosophy');
 
 ?>
