c71426974e9124f2a520ba68f71d554c2bc9c6e5
khantminkyi <khantminkyi@gmail.com>
2025-12-01 16:21:02 +0630

    change login credential from email to username and change admin sub menu

--- resources/js/components/nav-main.tsx
+++ resources/js/components/nav-main.tsx
@@ -1,8 +1,16 @@
-import { SidebarGroup, SidebarGroupLabel, SidebarMenu, SidebarMenuButton, SidebarMenuItem } from '@/components/ui/sidebar';
+import {
+    SidebarGroup,
+    SidebarGroupLabel,
+    SidebarMenu,
+    SidebarMenuButton,
+    SidebarMenuItem,
+    SidebarMenuSub,
+    SidebarMenuSubButton,
+    SidebarMenuSubItem,
+} from '@/components/ui/sidebar';
 import { type NavItem } from '@/types';
 import { Link, usePage } from '@inertiajs/react';
 import { ChevronDown } from 'lucide-react';
-import { Collapsible, CollapsibleContent, CollapsibleTrigger } from './ui/collapsible';
 
 export function NavMain({ items = [] }: { items: NavItem[] }) {
     const page = usePage();
@@ -13,33 +21,53 @@ export function NavMain({ items = [] }: { items: NavItem[] }) {
                 {items.map((item, i) => (
                     <div key={item.title}>
                         {item.haveSubItems ? (
-                            <Collapsible defaultOpen={page.url.startsWith(item.href)} className="group/collapsible">
-                                <SidebarGroup>
-                                    <SidebarGroupLabel asChild>
-                                        <CollapsibleTrigger>
-                                            {item.icon && <item.icon className="mr-2 text-zinc-900 dark:text-white" />}
-                                            <div className="text-sm text-zinc-900 dark:text-gray-100">{item.title}</div>
-                                            <ChevronDown className="ml-auto transition-transform group-data-[state=open]/collapsible:rotate-180" />
-                                        </CollapsibleTrigger>
-                                    </SidebarGroupLabel>
-                                    <CollapsibleContent>
-                                        {item.subItems?.map((subItem, key) => (
-                                            <SidebarMenuButton
-                                                asChild
-                                                key={key}
-                                                isActive={page.url.startsWith(subItem.href)}
-                                                tooltip={{ children: subItem.title }}
-                                                className="py-6"
-                                            >
+                            // <Collapsible defaultOpen={page.url.startsWith(item.href)} className="group/collapsible">
+                            //     <SidebarGroup>
+                            //         <SidebarGroupLabel asChild>
+                            //             <CollapsibleTrigger>
+                            //                 {item.icon && <item.icon className="mr-2 text-zinc-900 dark:text-white" />}
+                            //                 <div className="text-sm text-zinc-900 dark:text-gray-100">{item.title}</div>
+                            //                 <ChevronDown className="ml-auto transition-transform group-data-[state=open]/collapsible:rotate-180" />
+                            //             </CollapsibleTrigger>
+                            //         </SidebarGroupLabel>
+                            //         <CollapsibleContent>
+                            //             {item.subItems?.map((subItem, key) => (
+                            //                 <SidebarMenuButton
+                            //                     asChild
+                            //                     key={key}
+                            //                     isActive={page.url.startsWith(subItem.href)}
+                            //                     tooltip={{ children: subItem.title }}
+                            //                     className="py-6"
+                            //                 >
+                            //                     <Link href={subItem.href} prefetch>
+                            //                         {subItem.icon && <subItem.icon />}
+                            //                         <span>{subItem.title}</span>
+                            //                     </Link>
+                            //                 </SidebarMenuButton>
+                            //             ))}
+                            //         </CollapsibleContent>
+                            //     </SidebarGroup>
+                            // </Collapsible>
+                            <SidebarMenuItem>
+                                <SidebarMenuButton tooltip={{ children: item.title }} isActive={page.url.startsWith(item.href)} className="py-6">
+                                    {item.icon && <item.icon />}
+                                    <span>{item.title}</span>
+                                    <ChevronDown className="ml-auto transition-transform group-data-[state=open]/menuitem:rotate-180" />
+                                </SidebarMenuButton>
+
+                                <SidebarMenuSub>
+                                    {item.subItems?.map((subItem, key) => (
+                                        <SidebarMenuSubItem key={key}>
+                                            <SidebarMenuSubButton asChild isActive={page.url.startsWith(subItem.href)}>
                                                 <Link href={subItem.href} prefetch>
                                                     {subItem.icon && <subItem.icon />}
                                                     <span>{subItem.title}</span>
                                                 </Link>
-                                            </SidebarMenuButton>
-                                        ))}
-                                    </CollapsibleContent>
-                                </SidebarGroup>
-                            </Collapsible>
+                                            </SidebarMenuSubButton>
+                                        </SidebarMenuSubItem>
+                                    ))}
+                                </SidebarMenuSub>
+                            </SidebarMenuItem>
                         ) : (
                             <SidebarMenuItem>
                                 <SidebarMenuButton
