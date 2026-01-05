762bc198d15d5d42008d5012ba4410f5e4a15570
Khant Min Kyi <khantminkyi@gmail.com>
2026-01-05 19:15:09 +0630

    remove junk data

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -45,20 +45,6 @@
     <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@200..700&display=swap" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Roboto+Flex:opsz,wght@8..144,100..1000&display=swap"
         rel="stylesheet">
-    <link href="/assets/css/themify-icons.css" rel="stylesheet">
-    <link href="/assets/css/font-awesome.min.css" rel="stylesheet">
-    <link href="/assets/css/flaticon.css" rel="stylesheet">
-    <link href="/assets/css/magnific-popup.css" rel="stylesheet">
-    <link href="/assets/css/animate.css" rel="stylesheet">
-    {{-- <link href="/assets/css/owl.carousel.css" rel="stylesheet"> --}}
-    {{-- <link href="/assets/css/owl.theme.css" rel="stylesheet"> --}}
-    <link href="/assets/css/slick.css" rel="stylesheet">
-    <link href="/assets/css/slick-theme.css" rel="stylesheet">
-    <link href="/assets/css/swiper.min.css" rel="stylesheet">
-    <link href="/assets/css/jquery.fancybox.css" rel="stylesheet">
-    <link href="/assets/css/odometer-theme-default.css" rel="stylesheet">
-    {{-- <link href="assets/css/global.css" rel="stylesheet"> --}}
-    {{-- <link href="assets/css/style.css" rel="stylesheet"> --}}
     @routes
     @viteReactRefresh
     @vite(['resources/js/app.tsx', "resources/js/pages/{$page['component']}.tsx"])
@@ -68,15 +54,5 @@
 <body class="font-sans antialiased">
     @inertia
 </body>
-<!-- All JavaScript files
-    ================================================== -->
-<script src="/assets/js/jquery.min.js"></script>
-<!-- Plugins for this template -->
-<script src="/assets/js/modernizr.custom.js"></script>
-<script src="/assets/js/jquery-plugin-collection.js"></script>
-<script src="/assets/js/tw-elements.umd.min.js"></script>
-<script src="/assets/js/moving-animation.js"></script>
-<!-- Custom script for this template -->
-<script src="/assets/js/script.js"></script>
 
 </html>
